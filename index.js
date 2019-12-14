55a5e5a4559caa6883e9139052dfcad8fbeb8c39
elzoka <mahmoud_elzoka@yahoo.com>
2019-12-15 01:35:42 +0200

    create Bootcamp schema

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
 const routes = require('./routes');
 const connectDB = require('./config/db');
@@ -23,11 +24,11 @@ app.use(routes);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
 
 // handle rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red);
     server.close(() => process.exit(1));
 });
