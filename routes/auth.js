47135003272cf619d8ff7af061760bb7effc8d86
elzoka <mahmoud_elzoka@yahoo.com>
2020-01-13 15:17:10 +0200

    added read me and started config

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,16 @@ const router = require('express').Router();
 const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword, logout} = require('../controllers/auth');
 const {protect} = require('../middleware/auth');
 
+// Public
 router.post('/register', register);
 router.post('/login', login);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resetToken', resetPassword);
+
+// Private
 router.get('/logout', protect, logout);
 router.get('/me', protect, getMe);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
-router.post('/forgotpassword', forgotPassword);
-router.put('/resetpassword/:resetToken', resetPassword);
 
 module.exports = router;
